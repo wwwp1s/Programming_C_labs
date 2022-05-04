c56414be00660ae1c52493c399be7b98805a91a9
Zhulym Vladislav <kn1b21.zhulym@kpnu.edu.ua>
2022-05-04 17:16:34 +0300

    Lab01-0304

---
+++ Lab01/Lab01-0304/Lab01-0304.cpp
@@ -0,0 +1,22 @@
+﻿//Дана сторона рівностороннього трикутника. Знайти площу цього трикутника і радіуси
+//вписаного і описаного кіл.
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	double a, S, R, r, k;
+	cout << "введiть а: ";
+	cin >> a;
+	k = sqrt(3);
+	S = (pow(a, 2) * k) / 4;
+	R = (a / k);
+	r = (a / (2 * k));
+	cout << "S= " << S << endl;
+	cout << "R= " << R << endl;
+	cout << "r= " << r;
+	return 0;
+}
+
