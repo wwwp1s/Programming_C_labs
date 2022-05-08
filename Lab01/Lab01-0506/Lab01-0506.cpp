7b18a39348f7350cbbe96a79884a53d2edc16fc7
Zhulym Vladislav <kn1b21.zhulym@kpnu.edu.ua>
2022-05-08 23:13:33 +0300

    Lab01-0506

---
+++ Lab01/Lab01-0506/Lab01-0506.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    double z1, z2, x, y, iks, igryk;
+    cout << "введiть x: ";
+    cin >> x;
+    cout << "введiть y: ";
+    cin >> y;
+
+    iks = 2 * x;
+    igryk = 2 * y;
+
+    z1 = cos(pow(x, 4)) - cos(pow(y, 2)) + (sin(pow((iks), 2)) / 4);
+    z2 = (cos(iks) / 2) - (cos(igryk) / 2);
+    cout << "z1= " << z1 << endl;
+    cout << "z2= " << z2;
+
+    return 0;
+}
