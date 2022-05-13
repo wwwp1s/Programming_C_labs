87696030806719968ebf1ecce8f6f9d8b6866aff
Zhulym Vladislav <kn1b21.zhulym@kpnu.edu.ua>
2022-05-13 22:42:02 +0300

    Lab02-0205

---
+++ Lab02/Lab02-0205/Lab02-0205.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    float a, b, c;
+    double min, v1, v2;
+    cout << "введiть 3 числа через пробiл: ";
+    cin >> a >> b >> c;
+    v1 = ((a + b + c) / 2);
+    v2 = sqrt((1 / (pow(a, 2) + 1)) + (1 / (pow(b, 2) + 1)) + (1 / (pow(c, 2) + 1)));
+    min = v1;
+    if (v1 > v2)
+        cout << "min = " << v2;
+    else cout << "min = " << v1;
+
+    return 0;
+}
