7dcb88078d5e4ec04d3ecbad8bb5894d0f6e96be
Zhulym Vladislav <kn1b21.zhulym@kpnu.edu.ua>
2022-05-27 15:09:38 +0300

    MKR

---
+++ MKR/MKR.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    int S, T, v = 0;
+    cout << ("Введiть S: ");
+    cin >> S;
+    cout << ("Введiть T: ");
+    cin >> T;
+    v = S / T;
+    if (v >= 60)
+    {
+        printf("Водiй порушив правила");
+    }
+    else
+    {
+        printf("Водiй не порушував. v = %d", v);
+    }
+
+    return 0;
+}
---
+++ MKR1.2/MKR1.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    srand(time(0));
+    const int matrixSize = 5;
+    int matrix[matrixSize][matrixSize];
+
+    std::cout << "Matrix: " << std::endl;
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++)
+            std::cout << std::setw(5) << (matrix[i][j] = rand() % 25);
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < matrixSize; i++)
+        std::swap(matrix[0][i], matrix[matrixSize - 1][i]);
+
+    std::cout << "New matrix: " << std::endl;
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++)
+            std::cout << std::setw(5) << matrix[i][j];
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
